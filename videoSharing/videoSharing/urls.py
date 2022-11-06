61f1200fe31d5ed31e2a5a77ab5cc009b2ba483b
sanyuktajoshi <sanyukta.joshi13@gmail.com>
2022-11-06 14:34:44 +0530

    video view template added

--- videoSharing/videoSharing/urls.py
+++ videoSharing/videoSharing/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('videoapp/', include('videoapp.urls')),
@@ -23,6 +25,11 @@ urlpatterns = [
 ]
 
 handler404 = "videoapp.views.error404"
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
 
 
 
--- videoSharing/videoapp/urls.py
+++ videoSharing/videoapp/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('upload/', views.upload, name="UploadVideo"),
     path('login/', views.upload, name="login"),
 ]
+
+
 
